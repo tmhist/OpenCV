6908f6c7eed9697bd77bbedc325453bccc6b1754
tmhist <tmhist@gmail.com>
2023-10-06 20:56:45 +0200

    Added some changes

--- OpenCV_old.py
+++ OpenCV_old.py
@@ -1,34 +1,27 @@
 import cv2
 import pytesseract
 
-# Path to connect tesseract
-# pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+pytesseract.pytesseract_cmd = '/usr/local/bin/tesseract'
 
-# Connecting photo
-img = cv2.imread('/Users/timurmitrofanov/Desktop/Herder/TestImage.png')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = cv2.imread('/Users/path')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
 
-# Configures Tesseract OCR with specific settings for text extraction.
 config = r'--oem 3 --psm 6'
-# print(pytesseract.image_to_string(img, config=config))
 
-data = pytesseract.image_to_data(img, config=config)
+print(pytesseract.image_to_string(img, lang='rus', config=config))
 
-# Iterate through the data extracted from the image.
+data = pytesseract.image_to_data(img, lang='rus', config=config)
+
 for i, el in enumerate(data.splitlines()):
-	if i == 0:
-		continue
+    if i == 0:
+        continue
+    el = el.split()
+    try:
+        x, y, w, h = int(el[6]), int(el[7]), int(el[8]), int(el[9])
+        cv2.rectangle(img, (x, y), (w + x, h + y), (0, 0, 255), 1)
+        cv2.putText(img, el[11], (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, 1)
+    except IndexError:
+        print("Done")
 
-	el = el.split()
-	try:
-		# Creates captions on the picture
-		x, y, w, h = int(el[6]), int(el[7]), int(el[8]), int(el[9])
-		cv2.rectangle(img, (x, y), (w + x, h + y), (0, 0, 255), 1)
-		cv2.putText(img, el[11], (x, y), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 1)
-
-	except IndexError:
-		print("Operation was missed")
-
-# Displays results
 cv2.imshow('Result', img)
 cv2.waitKey(0)
